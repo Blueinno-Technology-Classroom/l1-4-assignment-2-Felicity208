6d6d45a2cf288e1fedc04c54d16dc4d14d765841
Felicity <felicity.curreem@gmail.com>
2025-01-15 22:01:37 +0800

    assignment1

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 # TODO: add your code here!
+number = int(input())
+count = 1
+sequence = []
+for i in range (number):
+    if len(sequence) <= 1:
+        count = 1
+    elif len(sequence) > 1:
+        count = (sequence[-2]+ sequence[-1])
+    sequence.append(count)
+print(sequence)
